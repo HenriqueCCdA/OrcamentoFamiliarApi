a2be7996f4ccdb514bf1644027eb52ad0a7bc545
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2022-01-23 22:35:39 -0300

    Listanado pelo metodo GET Receitas e Despesas
    close #7

--- orcamento_familiar_api/budget/views.py
+++ orcamento_familiar_api/budget/views.py
@@ -1,3 +1,33 @@
-# from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from orcamento_familiar_api.budget.models import Despesa, Receita
+
+
+def receitas(request):
+    income_list = Receita.objects.all()
+
+    income_list = [{
+                    'descricao': income.descricao,
+                    'valor': income.valor,
+                    'data': income.data
+                   }
+                   for income in income_list]
+
+    data = {'list': income_list}
+
+    return JsonResponse(data=data)
+
+
+def despesas(request):
+    outgoing_list = Despesa.objects.all()
+
+    outgoing_list = [{
+                      'descricao': outgoing.descricao,
+                      'valor': outgoing.valor,
+                      'data': outgoing.data
+                     }
+                     for outgoing in outgoing_list]
+
+    data = {'list': outgoing_list}
+
+    return JsonResponse(data=data)
