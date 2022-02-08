7210683efbfe047a03a1dce5acd60a25610b619e
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2022-02-07 23:35:43 -0300

    Criar rota para cadastramentos de receitas close #9

--- orcamento_familiar_api/budget/views.py
+++ orcamento_familiar_api/budget/views.py
@@ -1,23 +1,58 @@
+from json import loads
+from http import HTTPStatus
+from django.db import IntegrityError
+from datetime import datetime
+
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from orcamento_familiar_api.budget.models import Despesa, Receita
 
 
+@csrf_exempt
 def receitas(request):
-    income_list = Receita.objects.all()
 
-    income_list = [{
-                    'descricao': income.descricao,
-                    'valor': income.valor,
-                    'data': income.data
-                   }
-                   for income in income_list]
+    if request.method == 'GET':
+        income_list = Receita.objects.all()
 
-    data = {'list': income_list}
+        income_list = [{
+                        'descricao': income.descricao,
+                        'valor': income.valor,
+                        'data': income.data
+                      }
+                      for income in income_list]
 
-    return JsonResponse(data=data)
+        data = {'list': income_list}
+
+        return JsonResponse(data=data)
+
+    elif request.method == 'POST':
+        dict_ = loads(request.body)
+
+        # validantion
+        for key in Receita.required_fields():
+            if key not in dict_:
+                return JsonResponse(data={'error': f'"{key}" field is missing'},
+                                    status=HTTPStatus.BAD_REQUEST)
+
+        date = datetime.fromisoformat(dict_['data'])
+        receita = Receita(descricao=dict_['descricao'].strip(),
+                          valor=dict_['valor'],
+                          data=dict_['data'],
+                          mes=date.month)
+
+        try:
+            receita.save()
+        # restriticion unique_together = ('descricao', 'mes',)
+        except(IntegrityError):
+            return JsonResponse(data={'error': 'Income already registered'},
+                                status=HTTPStatus.CONFLICT)
+        return JsonResponse(data=dict_, status=HTTPStatus.CREATED)
+
+    return JsonResponse(data={})
 
 
+@csrf_exempt
 def despesas(request):
     outgoing_list = Despesa.objects.all()
 
