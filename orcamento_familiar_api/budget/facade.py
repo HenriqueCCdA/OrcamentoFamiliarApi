97329fc2972e18ab362ff330fb18637e7872b124
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2022-02-14 00:21:52 -0300

    Codigo sobrando na fachada retirado
    close #25

--- orcamento_familiar_api/budget/facade.py
+++ orcamento_familiar_api/budget/facade.py
@@ -1,17 +1,3 @@
-def check_restricion_create(model, obj):
-    return not model.objects.filter(descricao=obj.descricao, data__month=obj.data.month).exists()
-
-
-def check_restricion_update(model, obj):
-
-    exist_obj = model.objects.filter(descricao=obj.descricao, data__month=obj.data.month).first()
-
-    if not exist_obj:
-        return True
-
-    return obj.id == exist_obj.id
-
-
 def validation(model, data_in_request) -> str:
     '''
     Valida se existe algum campo faltando da requiscao
